07b5c0dccc0030c437295f4cb7fc18666db5647b
ErdoTez <53250803+ErdoTez@users.noreply.github.com>
2019-07-24 11:24:49 +0300

    Add files via upload

---
+++ KompleksSorgularınGenericHaleGelipCachlenmesi/KompleksSorgularınGenericHaleGelipCachlenmesi/IQueryableExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace KompleksSorgularınGenericHaleGelipCachlenmesi
+{
+    public static class IQueryableExtension
+    {
+        public static List<T> GetCache<T>(this IQueryable<T> query)
+        {
+            string sql = ((ObjectQuery)query).ToTraceString();
+            if (HttpContext.Current.Cache[sql] == null)
+            {
+                List<T> list = query.ToList();
+                HttpContext.Current.Cache.Insert(sql, list)
+            }
+            return HttpContext.Current.Cache[sql] as List<T>;
+        }
+    }
+}
